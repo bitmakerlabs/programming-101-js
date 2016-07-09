769df114c011eddc6228fcd772feec81cc1408db
Fabio Gelsomini <fgelsomini@gmail.com>
2016-07-09 08:55:21 -0400

    Add event exercises

--- js/todos.js
+++ js/todos.js
@@ -1,3 +1,17 @@
+// First let's practice using events. 
+// TIP - Comment out the previous exercise when starting the next one
+
+// 1. Get the browser to write to the console when someone clicks anywhere in the document
+
+// 2. Change the text of the h1 that displays the page title when someone clicks anywhere in the page
+
+// 3. Display an alert when someone clicks any of the tasks
+
+// 4a. Write a function that displays an alert
+// 4b. Run the function only when any task is clicked
+
+
+// Now let's get the application to do what it needs to do
 // Write a function that updates the counter based on which tasks are completed
 
 
